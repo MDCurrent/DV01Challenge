fdf7627bf887a542ecc74c21dac7df8fe311a65d
Matthew Current <currentmatthew@gmail.com>
2019-02-14 15:58:56 -0500

    Added X axis Labels and tick marks. plots data in order by month

---
+++ produceVisualizations.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def monthNuber(issue_d):
+    monthYear = issue_d.split('-')
+    months= ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    offset = int(monthYear[1]) - 2008
+    return offset * 12 + months.index(monthYear[0]) + 1
+
+def getTickLabels(orderedGradeRows):
+    interestingrows = [orderedGradeRows.loc[orderedGradeRows['order'] == i] for i in range(0, len(orderedGradeRows)) if i % int((len(orderedGradeRows)/6)) == 0]
+    monthlabels = [str(interestingrows[i]['issue_d']).split()[1] for i in range(len(interestingrows)-1)]
+    return monthlabels
+
+
+
+csv = pd.read_csv('LoanStats3a.csv', low_memory=False)
+gradeSumByDate = csv.groupby(['grade', 'issue_d']).agg(['sum', 'count', 'mean'])['loan_amnt'].reset_index()
+orderedList = [monthNuber(issue_d=i) for i in gradeSumByDate['issue_d']]
+gradeSumByDate['order'] = pd.Series(orderedList)
+gradeSumByDate = gradeSumByDate.sort_values(by='order')
+meanplot = gradeSumByDate.pivot(index='order', columns='grade', values='mean').plot()
+meanplot.set_xlabel('Issue Month')
+meanplot.set_xticklabels(getTickLabels(gradeSumByDate.loc[gradeSumByDate['grade'] == 'A'].sort_values('order')))
+sumplot = gradeSumByDate.pivot(index='issue_d', columns='grade', values='sum').plot()
+sumplot.set_xlabel('Issue Month')
+sumplot.set_xticklabels(getTickLabels(gradeSumByDate.loc[gradeSumByDate['grade'] == 'A'].sort_values('order')))
+countplot = gradeSumByDate.pivot(index='issue_d', columns='grade', values='count').plot()
+countplot.set_xlabel('Issue Month')
+countplot.set_xticklabels(getTickLabels(gradeSumByDate.loc[gradeSumByDate['grade'] == 'A'].sort_values('order')))
+countplot.figure.savefig('maeanplot.png')
+sumplot.figure.savefig('sumplot.png')
+countplot.figure.savefig('countplot.png')
+meanplot.figure.show()
+countplot.figure.show()
+sumplot.figure.show()
